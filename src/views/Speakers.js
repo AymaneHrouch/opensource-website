2e1cb8b02826a99e53d08511319d33388e889692
Aymane Hrouch <jo.aymane@Gmail.com>
2022-05-16 07:07:28 +0000

    update guests infos

--- src/views/Speakers.js
+++ src/views/Speakers.js
@@ -7,43 +7,51 @@ import image3 from "../assets/Speakers/Larbi OUIYZME.png";
 import image4 from "../assets/Speakers/Badr Boussabat.png";
 import image5 from "../assets/Speakers/Mehdi CHALLIGUI.png";
 import image6 from "../assets/Speakers/Amadou Diawara.png";
+import ikbal from "../assets/Speakers/ilhamikbal.png";
+import moumni from "../assets/Speakers/moumni.png";
+import ayoub from "../assets/Speakers/ayoub.png";
 import unknown from "../assets/Team/comite_os_10/unknown.jpg";
 
 const speakers = [
     {
+        image: ayoub,
+        name: "KBAILI Ayoub",
+        introduction: "Data & AI Lead chez Microsoft Maroc",
+    },
+    {
         image: image2,
-        name: "Nadia NAANAA",
+        name: "NAANAA Nadia",
         introduction: "Consultante Senior/Formatrice MSBI, Power BI",
     },
     {
         image: image3,
-        name: "Larbi OUIYZME",
+        name: "OUIYZME Larbi",
         introduction: `Consultant Systèmes, Réseaux et Cybersécurité ,
         Consultant / Formateur Technique francophone spécialisé sur les produits de la technologie Microsoft`,
     },
     {
         image: image4,
-        name: "Badr Boussabat",
+        name: "BOUASSABAT Badr",
         introduction: "President and founder of AI TOGETHER",
     },
     {
         image: image5,
-        name: "Mehdi CHALLIGUI",
+        name: "CHALLIGUI Mehdi",
         introduction: `President of the Comission of the Promotion of Digital and Technologies`,
     },
     {
         image: image6,
-        name: "Amadou Diawara",
+        name: "DIAWARA Amadou",
         introduction: `President and Founder of Cluster Digital Africa & the 
         CEO of the Center for Innovation in Technological Research and Creative Industry (CIRTIC) MBA Management and management of IT projects`,
     },
     {
-        image: unknown,
-        name: "Amina Moumni",
+        image: moumni,
+        name: "MOUMNI Amina",
     },
     {
-        image: unknown,
-        name: "Ilham Ikbal",
+        image: ikbal,
+        name: "IKBAL Ilham",
         introduction: `ingenieur cybersécurité senior chez IDEMIA`,
     },
 ];
@@ -86,7 +94,18 @@ const Speakers = () => {
             </div>
             <Grid container justify="center">
                 {speakers.map((speaker, index) => (
-                    <Grid item lg={3} md={4} xs={6} container direction="column" justify="center" alignItems="center" key={index} className={classes.speakerContainer}>
+                    <Grid
+                        item
+                        lg={3}
+                        md={4}
+                        xs={6}
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                        key={index}
+                        className={classes.speakerContainer}
+                    >
                         <Avatar className={classes.image} src={speaker.image} alt="speaker" />
                         <Typography className={classes.speakername} variant="h4">
                             {speaker.name}
